2851079437eb09083dfdb4b8f121ec15c3fef668
Jeong mokjin <2022301072@skuniv.ac.kr>
2025-07-06 23:36:49 +0900

    파일 추가

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+function Header(){
+    return(
+        <>
+        <Head>
+            헤드
+        </Head>
+        </>
+    );
+}
+
+export default Header
+
+const Head=styled.div`
+    display: flex;
+    background-color: red;
+`
